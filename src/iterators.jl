bf70cfdfddc08e77693f135014a322a7592b5cb3
Tomas Votroubek <tomas.votroubek@outlook.com>
2023-09-28 22:18:29 +0200

    limited eps?

--- src/iterators.jl
+++ src/iterators.jl
@@ -1,6 +1,7 @@
-using Base.Iterators: dropwhile, drop
+using Base.Iterators: dropwhile, drop, take
 
 dropwhile_enumerate(pred, itr) = dropwhile(x -> pred(x[2]), enumerate(itr))
 
+limited_eps(xs, gap, n) = first(dropwhile_enumerate(x -> max_incentive(x) > gap, take(xs, n)))
 until_eps(xs, gap) = first(dropwhile_enumerate(x -> max_incentive(x) > gap, xs))
 fixed_iters(d, i) = first(drop(d, i))
